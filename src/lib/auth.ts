9faca7b15dc8a2bfbe2660cdc3928149dc37847a
Pranusha <bellepranusha@gmail.com>
2026-05-29 17:03:06 +0530

    Replace bcrypt with bcryptjs to resolve slow login and signup times

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { createServerFn } from "@tanstack/react-start";
 import { setCookie, getCookie, deleteCookie } from "@tanstack/react-start/server";
 import { SignJWT, jwtVerify } from "jose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { saveUser, getUserByEmail, User } from "../server/db";
 
 export type AuthUser = {
